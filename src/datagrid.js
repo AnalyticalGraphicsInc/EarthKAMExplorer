3af4fe626f48c81400c5dd558e62637f56b2be6d
hpinkos <pinkos.hannah@gmail.com>
2013-04-23 13:56:34 -0400

    improved table visually, added library for selecting and adding buttons, added minimize button.

--- src/datagrid.js
+++ src/datagrid.js
@@ -1,7 +1,7 @@
 /*global define*/
-define(['../jQuery/jQuery', '../jQuery/jquery.dataTables'], function(jQuery,dataTable) {
+define(['TableTools'], function(TableTools) {
     "use strict";
-    
+
     var createGrid = function(ISS_Data) {
         $('#grid_search').html( '<table cellpadding="0" cellspacing="0" border="0" class="display" id="example"></table>' );
         $('#example').dataTable( {
@@ -12,6 +12,28 @@ define(['../jQuery/jQuery', '../jQuery/jquery.dataTables'], function(jQuery,data
             "bScrollCollapse": true,
             "sScrollY": "200px",
             "bInfo": false,
+            "sDom": '<"H"Tfr>t<"F"ip>',
+            "oTableTools": {
+                "sRowSelect": "single",
+                "aButtons": [{
+                    "sExtends": "text",
+                    "sButtonText": '<img src="/jQuery/css/down.png"/>',
+                    "sButtonClass" : "minus",
+                    "fnClick": function(nButton){
+                        $('#grid_search').toggleClass('minimized');
+
+                        if (nButton.classList[3] === 'minus') {
+                            nButton.innerHTML = '<span><img src="/jQuery/css/up.png"/></span>';
+                            $('#example_filter').css('display', 'none');
+                        } else {
+                            nButton.innerHTML = '<span><img src="/jQuery/css/down.png"/></span>';
+                            $('#example_filter').css('display', 'block');
+                        }
+                        $('#ToolTables_example_0').toggleClass('plus').toggleClass('minus');
+                        $('#ToolTables_example_0').parent().parent().toggleClass('ui-corner-bl').toggleClass('ui-corner-br');
+                    }
+                }]
+            },
             "aoColumns": [
                 { "sTitle": "ID", "bVisible": false},
                 { "sTitle": "Time" },
@@ -19,13 +41,8 @@ define(['../jQuery/jQuery', '../jQuery/jquery.dataTables'], function(jQuery,data
                 { "sTitle": "School" }
             ],
             "aaSorting": [[ 1, "desc" ]]
-        } );	
+        } );
     }
 
     return createGrid;
 });
-
-
-
-        
-        
