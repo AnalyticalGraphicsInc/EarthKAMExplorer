aaa643d118f54341240aff2201ad1d29d74cd1d8
Dan Bagnell <daniel.p.bagnell@gmail.com>
2013-04-23 17:15:29 -0400

    Add new zooming.

--- src/onFrameChanged.js
+++ src/onFrameChanged.js
@@ -3,11 +3,11 @@ define(['require'], function(require) {
     "use strict";
     /*global Cesium*/
 
-
     var width = 600.0 * 100.0;
     var height = 600.0 * 100.0;
 
-    var radius = Cesium.Ellipsoid.WGS84.getMaximumRadius();
+    var ellipsoid = Cesium.Ellipsoid.WGS84;
+    var radius = ellipsoid.getMaximumRadius();
     var rotateFactor = 1.0 / radius;
     var rotateRateRangeAdjustment = radius;
     var maximumRotateRate = 1.77;
@@ -41,6 +41,50 @@ define(['require'], function(require) {
         controller.rotateUp(deltaTheta);
     }
 
+    var depth = 600.0 * 100.0;
+
+    var zoomFactor = 5.0;
+    var minimumZoomRate = 20.0;
+    var maximumZoomRate = Cesium.FAR;
+    var minimumZoomDistance = 20.0;
+    var maximumZoomDistance = Number.POSITIVE_INFINITY;
+
+    function zoom(scene, z) {
+        var camera = scene.getCamera();
+        var controller = camera.controller;
+
+        var height = ellipsoid.cartesianToCartographic(camera.position).height;
+
+        // distanceMeasure should be the height above the ellipsoid.
+        // The zoomRate slows as it approaches the surface and stops minimumZoomDistance above it.
+        var minHeight = minimumZoomDistance;
+        var maxHeight = maximumZoomDistance;
+
+        var minDistance = height - minHeight;
+        var zoomRate = zoomFactor * minDistance;
+        zoomRate = Cesium.Math.clamp(zoomRate, minimumZoomRate, maximumZoomRate);
+
+        var rangeWindowRatio = z / depth;
+        rangeWindowRatio = Math.min(rangeWindowRatio, maximumMovementRatio);
+        var distance = zoomRate * rangeWindowRatio;
+
+        if (distance > 0.0 && Math.abs(height - minHeight) < 1.0) {
+            return;
+        }
+
+        if (distance < 0.0 && Math.abs(height - maxHeight) < 1.0) {
+            return;
+        }
+
+        if (height - distance < minHeight) {
+            distance = height - minHeight - 1.0;
+        } else if (height - distance > maxHeight) {
+            distance = height - maxHeight;
+        }
+
+        controller.zoomIn(distance);
+    }
+
     var firstFrame;
 
     function onFrameChanged(scene, frame) {
@@ -51,6 +95,7 @@ define(['require'], function(require) {
 
           var translation = firstFrame.translation(frame);
           rotate(scene, translation[0], translation[1]);
+          zoom(scene, translation[2]);
         }
     }
 
