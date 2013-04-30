1a07e18e4d31126da5fb0f66ce34273c6b7103f0
Scott Hunter <shunter@agi.com>
2013-04-30 10:39:10 -0400

    Don't submit node modules.  Update instructions to run `npm install` first.

--- server/server.js
+++ server/server.js
@@ -13,29 +13,11 @@
     var url = require('url');
     var request = require('request');
 
-    var Q = require('q');
-
     var dir = path.join(__dirname, '..');
 
     var app = express();
     app.use(express.compress());
     app.use(express.static(dir));
-
-    var nano = require('nano')('http://localhost:5984');
-
-    function getDatabase() {
-        var name = 'space_apps';
-
-        return Q.nfcall(nano.db.get, name).then(function() {
-            return nano.use(name);
-        }, function(error) {
-            return Q.nfcall(nano.db.create, name).then(function() {
-                return nano.use(name);
-            }, function(error) {
-                console.log(error);
-            });
-        });
-    }
 
     app.get('/proxy', function(req, res) {
         var remoteUrl = Object.keys(req.query)[0];
@@ -44,14 +26,6 @@
         }
 
         request.get(remoteUrl).pipe(res);
-    });
-
-    app.get('/rows', function(req, res) {
-        return getDatabase().then(function(db) {
-            return Q.nfcall(db.list);
-        }).then(function(body) {
-            res.send(body[0].rows.join(','));
-        }).done();
     });
 
     app.listen(8080);
